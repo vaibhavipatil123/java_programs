7acc39519cdfa4863daacf769c5adecf0d69ac67
Vaibhavi Patil <105306254+vaibhavipatil123@users.noreply.github.com>
2022-09-12 17:22:37 +0530

    Update problem4.java

--- problem4.java
+++ problem4.java
@@ -6,18 +6,19 @@ import java.util.Scanner;
 
 class VowelNotAllowedException extends Exception{
     public String toString(){
-        return "Exception: Vowels are not allowed";
+        return "Exceptio: vowel not allowed";
     }
+
 }
 
 public class question4 {
     public static void main(String[] args) throws IOException, VowelNotAllowedException {
         
         //File file=new File("C://Users\//HP//OneDrive//Desktop//AOOP//read.txt");
-        File file1=new File("C:\\Users\\HP\\OneDrive\\Desktop\\AOOP\\alphabet.txt");
-        FileReader fr=new FileReader("C:\\Users\\HP\\OneDrive\\Desktop\\AOOP\\alphabet.txt");
+        File file1=new File("alphabet.txt");
+        FileReader fr=new FileReader("alphabet.txt");
         //File file2=new File("C:\\Users\\HP\\OneDrive\\Desktop\\AOOP\\write.txt");
-        FileWriter fw=new FileWriter("C:\\Users\\HP\\OneDrive\\Desktop\\AOOP\\consonants.txt");
+        FileWriter fw=new FileWriter("consonants.txt");
 
         try{
             file1.createNewFile();
@@ -31,18 +32,22 @@ public class question4 {
 
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
+
             try{
-                if((ch=='a') ||(ch=='e')|| (ch=='i') || (ch=='o') || (ch=='u')){
+                if((ch=='a') ||(ch=='e')|| (ch=='i') || (ch=='o') || (ch=='u') || (ch=='A') || (ch=='E') || (ch=='I') || (ch=='O') || (ch=='U')){
                     throw new VowelNotAllowedException();
                 }
                 else{
-                    
+                    System.out.println(ch +" coppied to consonants.txt");
                     fw.write(ch);
-                    System.out.println("coppied");
+                    
                 }
             }catch(Exception ee){
                 System.out.println(ee);
             }
+           
         }
+         fw.close();
     } 
 }
+
